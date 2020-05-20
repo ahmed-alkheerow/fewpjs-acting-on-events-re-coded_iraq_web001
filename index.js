8787d826af21c55954ae411bfda46a89d2f5828a
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-05-20 03:07:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function moveDodgerLeft() {
 function moveDodgerRight() {
   let rightNumbers = dodger.style.right.replace("px", "");
   let right = parseInt(rightNumbers, 10);
-
+dodger.style.left='';
   if (right > 0) {
     dodger.style.right = `${right - 1}px`;
   }
