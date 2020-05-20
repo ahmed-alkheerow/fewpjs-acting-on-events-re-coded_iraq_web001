497bd579c2f16f130b7b8740c704808964bc239f
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-05-20 03:10:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,12 @@ function moveDodgerRight() {
     dodger.style.left = `${left + 1}px`;
   }
 }
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+ 
+    dodger.style.left = `${left - 1}px`;
+  }
+});
